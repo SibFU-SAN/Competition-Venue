0a2cb5f34153ee37b39c2678484e5400973f1ba4
Chevik08 <76528394+Chevik08@users.noreply.github.com>
2021-03-17 10:10:22 +0700

    Postdeath update
    Исправлена ошибка при попытке игрока ходить после смерти змейки

--- snake/snake_controls.py
+++ snake/snake_controls.py
@@ -1,4 +1,5 @@
 from snake_game import SnakePlayer, Map
+import copy
 
 
 server_info = []
@@ -10,42 +11,47 @@ direction = 1
 
 def move_left():
     global snake_position, direction, apples, map_1, server_info
-    snake_position, direction = snake_position, direction\
-        = SnakePlayer(map_1, snake_position, apples, direction).\
-        snake_move_around(3)
-    server_info.append(snake_position)
+    if ['stop'] not in snake_position:
+        snake_position, direction = snake_position, direction\
+            = SnakePlayer(map_1, snake_position, apples, direction).\
+            snake_move_around(3)
+        server_info.append(copy.deepcopy(snake_position))
 
 
 def move_up():
     global snake_position, direction, apples, map_1, server_info
-    snake_position, direction = snake_position, direction\
-        = SnakePlayer(map_1, snake_position, apples, direction).\
-        snake_move_around(1)
-    server_info.append(snake_position)
+    if ['stop'] not in snake_position:
+        snake_position, direction = snake_position, direction\
+            = SnakePlayer(map_1, snake_position, apples, direction).\
+            snake_move_around(1)
+        server_info.append(copy.deepcopy(snake_position))
 
 
 def move_down():
     global snake_position, direction, apples, map_1, server_info
-    snake_position, direction = snake_position, direction\
-        = SnakePlayer(map_1, snake_position, apples, direction).\
-        snake_move_around(2)
-    server_info.append(snake_position)
+    if ['stop'] not in snake_position:
+        snake_position, direction = snake_position, direction\
+            = SnakePlayer(map_1, snake_position, apples, direction).\
+            snake_move_around(2)
+        server_info.append(copy.deepcopy(snake_position))
 
 
 def move_right():
     global snake_position, direction, apples, map_1, server_info
-    snake_position, direction = snake_position, direction\
-        = SnakePlayer(map_1, snake_position, apples, direction).\
-        snake_move_around(4)
-    server_info.append(snake_position)
+    if ['stop'] not in snake_position:
+        snake_position, direction = snake_position, direction\
+            = SnakePlayer(map_1, snake_position, apples, direction).\
+            snake_move_around(4)
+        server_info.append(copy.deepcopy(snake_position))
 
 
 def move():
     global snake_position, direction, apples, map_1, server_info
-    snake_position, direction = snake_position, direction \
-        = SnakePlayer(map_1, snake_position, apples, direction). \
-        snake_move_forward()
-    server_info.append(snake_position)
+    if ['stop'] not in snake_position:
+        snake_position, direction = snake_position, direction \
+            = SnakePlayer(map_1, snake_position, apples, direction). \
+            snake_move_forward()
+        server_info.append(copy.deepcopy(snake_position))
 
 
 for i in range(2):
@@ -54,3 +60,7 @@ move_up()
 move_up()
 move_right()
 move()
+move_up()
+move_up()
+"Попытка ходить после смерти"
+move_up()
