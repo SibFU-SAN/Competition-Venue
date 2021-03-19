1cd1b686dd7af21a7d0dc58ac044c8bea5b43081
qPexLegendary <terentev.andrey.2002@gmail.com>
2021-03-19 15:31:05 +0700

    Добавлена регистрация и вход + помечены страницы, которые требуют вход пользователя

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import flask
 
 from json.encoder import JSONEncoder
-from modules import methods
+from modules import methods, account_methods
 
 
 json_encoder = JSONEncoder()
@@ -37,16 +37,19 @@ def index_page():
 
 
 @app.route("/game/connect", methods=["POST", "GET"])
+@account_methods.authorize_require
 def game_connect_page():
     return flask.render_template("pages/game/connect.html")
 
 
 @app.route("/game/create", methods=["POST", "GET"])
+@account_methods.authorize_require
 def game_create_page():
     return flask.render_template("pages/game/create.html")
 
 
 @app.route("/game/editor", methods=["POST", "GET"])
+@account_methods.authorize_require
 def game_editor_page():
     return flask.render_template("pages/game/editor.html")
 
@@ -62,17 +65,31 @@ def info_guide_page():
 
 
 @app.route("/profile", methods=["POST", "GET"])
+@account_methods.authorize_require
 def profile_page():
     return flask.render_template("pages/profile/index.html")
 
 
 @app.route("/login", methods=["POST", "GET"])
+@account_methods.not_authorize_require
 def login_page():
+    data = methods.login(**flask.request.form)
+    if data['type'] == 'success':
+        response = flask.make_response(flask.redirect("/profile"))
+        response.set_cookie("auth", value=data['object']['token'], max_age=60*60*24*7)
+        print(data['object']['token'])
+        return response
     return flask.render_template("pages/profile/login.html")
 
 
 @app.route("/register", methods=["POST", "GET"])
+@account_methods.not_authorize_require
 def register_page():
+    data = methods.register(**flask.request.form)
+    if data['type'] == 'success':
+        response = flask.make_response(flask.redirect("/profile", 302))
+        response.set_cookie("auth", value=data['object']['token'], max_age=60*60*24*7)
+        return response
     return flask.render_template("pages/profile/register.html")
 
 
