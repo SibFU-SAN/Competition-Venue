963cd51c9a8ef4e4579c7670393a0dae8a45c928
Bochkarev Alexandr <bochkarev2002@bk.ru>
2021-03-31 13:55:29 +0700

    Добавил шаблон стр для просмотра игр

--- templates/static/game.js
+++ templates/static/game.js
@@ -1,36 +1,45 @@
-/* Размеры поля в клетках */
-let height = 32;
-let width = 48;
-
-const map = new Array(height);
-for (let i = 0; i < height; i++)
-	map[i] = new Array(width);
-
+// Связь с htm
 const canvas  = document.getElementById("game_screen");
 const ctx = canvas.getContext('2d');
 
+let gameStopped = true;
+const btnStop  = document.getElementById("stop");
+btnStop.addEventListener("click", stopButtonClick);
+
+// Карта Игры
+const height = 32;
+const width = 48;
+
+let step_x = Math.floor(canvas.width / width);
+let step_y = Math.floor(canvas.height / height);
+
+canvas.width = width * step_x;
+canvas.height = height * step_y;
+
+/* Наброски
+const map = new Array(height);
+for (let i = 0; i < height; i++)
+	map[i] = new Array(width);
+*/
+
+
 function drawField() {
 	/* Рисуем поле */
-	let step;
-	
-	ctx.fillStyle = "#abff96";
+	ctx.fillStyle = "#8cca88";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	
-	ctx.fillStyle = "#000000";
-	
-	step = Math.floor(canvas.width / width);
-	for (j = step; j < canvas.width; j += step) {
+
+	// ?
+	ctx.strokeStyle = "#77b373";
+	for (let j = step_x; j < canvas.width; j += step_x) {
 		ctx.beginPath();
-		ctx.moveTo(j, 0);  
+		ctx.moveTo(j, 0);
 		ctx.lineTo(j, canvas.height);
 		ctx.closePath();
 		ctx.stroke();
 	}
-	
-	step = Math.floor(canvas.height / height);
-	for (j = step; j < canvas.height; j += step) {
+	for (let j = step_y; j < canvas.height; j += step_y) {
 		ctx.beginPath();
-		ctx.moveTo(0, j);  
+		ctx.moveTo(0, j);
 		ctx.lineTo(canvas.width, j);
 		ctx.closePath();
 		ctx.stroke();
@@ -39,28 +48,96 @@ function drawField() {
 
 function drawFood(x, y) {
 	/* Рисуем еду */
-	let step_x = Math.floor(canvas.width / width);
-	let step_y = Math.floor(canvas.height / height);
 	let radius = Math.floor(Math.min(step_x, step_y) / 2);
-
-	ctx.fillStyle = '#ff9090';
+	ctx.fillStyle = '#00ff00';
 	ctx.beginPath();
 	ctx.arc((x + 0.5) * step_x, (y + 0.5) * step_y, radius, 0, 2*Math.PI, false);
 	ctx.closePath();
 	ctx.fill();
 }
 
-
-function drawGame() {
-	/* Нарисовать поле */
-	drawField();
-
-	/* Пройтись по массиву, отображая каждый элемент */
-	drawFood(5, 5);
-
-	/* Взять следующий "кадр" */
+function drawSnakeElement(x, y) {
+	ctx.fillRect(x * step_x, y * step_y, step_x, step_y);
 }
 
-drawGame();
+function drawSnakeHead(x, y) {
+	ctx.fillRect(x * step_x, y * step_y, step_x, step_y);
 
-//let game = setInterval(drawGame, 500);
+	let tempColor = ctx.fillStyle;
+	ctx.fillStyle = "#000000";
+
+	ctx.beginPath();
+	let radius = Math.floor(Math.min(step_x, step_y) / 2);
+	ctx.arc((x + 0.5) * step_x, (y + 0.5) * step_y, radius/2, 0, 2*Math.PI, false);
+	ctx.closePath();
+	ctx.fill();
+
+	ctx.fillStyle = tempColor;
+}
+
+function drawSnake() //  user_id, snakeCoordinates
+{
+	let isFriend = false;
+	if (true) // TODO: Добавить проверку на пользователя
+		isFriend = true;
+
+	const friendSnake = '#57ff64';
+	const enemySnake = '#f64d27';
+
+	if (isFriend)
+		ctx.fillStyle = friendSnake;
+	else
+		ctx.fillStyle = enemySnake;
+
+	//
+	drawSnakeHead(1, 1, isFriend);
+	drawSnakeElement(2, 1, isFriend);
+	drawSnakeElement(3, 1, isFriend);
+	drawSnakeElement(4, 1, isFriend);
+}
+
+function stopButtonClick() {
+	if (gameStopped) {
+		btnStop.value = "Остановить";
+	} else {
+		btnStop.value = "Запуск";
+	}
+	gameStopped = !gameStopped;
+}
+
+function drawGame() {
+	if (gameStopped) return;
+
+	// получить данные кадра
+	// ...
+
+	let command = gameReplay[stepCount];
+	let command_context = "";
+
+	// отрисовака кадра
+	drawField();
+
+	if (command == "s") {
+		drawSnake();
+	} else if (command == "a") {
+		drawFood(3 + stepCount,3 + stepCount);
+	}
+
+	// ...
+	if (stepCount < maxStep - 1) stepCount += 1;
+}
+
+function main(){
+	drawField();
+	drawFood(0,0);
+	drawFood(width - 1, height - 1);
+
+	let game = setInterval(drawGame, 1000);
+}
+
+gameReplay = "sa";
+let maxStep = gameReplay.length;
+let stepCount = 0;
+
+main();
+
