22cfcab25ac8b46a491939f582b233fb1e2b2d90
qPexLegendary <terentev.andrey.2002@gmail.com>
2021-05-05 17:40:20 +0700

    Сделана основа для запуска проверки игры

--- modules/game_handler.py
+++ modules/game_handler.py
@@ -1,6 +1,8 @@
 import time
 from threading import Thread
+import asyncio
 from logging import Logger
+from database import db_get_ended_games
 
 
 class GameHandler(Thread):
@@ -11,6 +13,11 @@ class GameHandler(Thread):
     def run(self) -> None:
         self.logger.info("Запуск обработчика игр")
         while True:
+            games = db_get_ended_games()
 
-            # TODO: Обработка игр
+            asyncio.run(self.handle(games))
             time.sleep(5)
+
+    async def handle(self, games: list):
+        # TODO: Обработка игр
+        pass
