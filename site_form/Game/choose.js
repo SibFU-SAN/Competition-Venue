045bcceaac46b13c9777d59caad2e2920b061023
Bochkarev Alexandr <bochkarev2002@bk.ru>
2021-05-10 17:55:48 +0700

    Добавлени функция вствки сессии в html список на сайте

--- site_form/Game/choose.js
+++ site_form/Game/choose.js
@@ -11,41 +11,55 @@ let sessionList = [
 {
 	id: 131232,
 	map: [10, 100],
-	gamers: [5, 20],
+	gamers: 5,
 	date: "this date",
 	duration: 34,
 },
 {
 	id: 243534,
 	map: [10, 100],
-	gamers: [5, 20],
+	gamers: 20,
 	date: "this date",
 	duration: 34,
 }
 ];
 
-let template = `
-	<div class="session">
-		<div class="session-head"> <!-- Загаловок -->
-			<h3>ID: <span>23112</span></h3>
-			<input type="button" value="Выбор" class="btn" onclick="chooseGame()">
+
+function createTemplate(game_id, NumberOfPlayers, mapSize, startDate, duration) {
+	let template = `
+		<div class="session">
+			<div class="session-head">
+				<h3>ID: <span>${game_id}</span></h3>
+				<input type="button" value="Выбор" class="btn" onclick="chooseGame()">
+			</div>
+			<div class="session-info">
+				<ul>
+					<li>Колво участников: <span>${NumberOfPlayers}</span></li>
+					<li>Размер карты: <span>${mapSize[0]}x${mapSize[1]}</span></li>
+				</ul>
+				<ul>
+					<li>Дата начала: <span>${startDate}</span></li>
+					<li>Длительность: <span>${duration} min</span></li>
+				</ul>
+			</div>
 		</div>
-		<div class="session-info"> <!-- Информаця о игре -->
-			<ul>
-				<li>Колво участников: <span>44</span></li>
-				<li>Размер карты: <span>44x44</span></li>
-			</ul>
-			<ul>
-				<li>Дата начала: <span>10.10.2021</span></li>
-				<li>Длительность: <span>45 min</span></li>
-			</ul>
-		</div>
-	</div>
-	`;
+		`;
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = template;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
+}
 
 function main()
 {
-	document.createElement(template);
+	for (i of sessionList)
+	{
+		var fragment = createTemplate(1111, 44, [50, 100], "13.23.22", 50);
+		document.getElementById("session_list").appendChild(fragment);
+	}
 }
 
 main();
