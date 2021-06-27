c0fdbee2c3a9d89fe05df699c1915093a2ac42ae
qPexLegendary <terentev.andrey.2002@gmail.com>
2021-06-27 14:48:49 +0700

    Дополнен шаблон

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `users` (
-    `id` int(5) UNIQUE NOT NULL,
+    `id` int(5) UNIQUE NOT NULL AUTO_INCREMENT,
     `login` varchar(32) UNIQUE NOT NULL,
     `password` varchar(32) NOT NULL,
     `wins` int NOT NULL DEFAULT 0,
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 );
 
 CREATE TABLE IF NOT EXISTS `games` (
-    `id` int(5) UNIQUE NOT NULL,
+    `id` int(5) UNIQUE NOT NULL AUTO_INCREMENT,
     `name` tinytext NOT NULL,
     `start_time` int NOT NULL,
     `end_time` int NOT NULL,
@@ -16,12 +16,13 @@ CREATE TABLE IF NOT EXISTS `games` (
     `owner` int(5) NOT NULL,
     `status` tinyint NOT NULL DEFAULT 0,
     `private` boolean NOT NULL,
+    `mode` tinyint NOT NULL,
     `settings` JSON NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS `players` (
-    `id` int(5) UNIQUE NOT NULL,
+    `id` int(5) UNIQUE NOT NULL AUTO_INCREMENT,
     `game` int(5) NOT NULL,
     `user` int(5) NOT NULL,
     PRIMARY KEY(id)
